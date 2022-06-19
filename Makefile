a1e7b40c50a185988f11a0b0549519e20acb26ce
marouane-youssfi10 <marouaneyoussfi5@gmail.com>
2022-06-19 18:49:09 +0100

    setup envirement of docker & django

---
+++ Makefile
@@ -0,0 +1,61 @@
+help:
+	@echo "docker-compose commands:"
+	@echo "    make lint:         Run pre-commit hooks"
+	@echo "    make rm: 		  Removes all docker containers running"
+	@echo "    make up: 		  Up all services - Starts all services"
+	@echo "    make bash: 		  Bash into application container"
+	@echo "    make ps:			  list containers/services"
+	@echo "	   make restart:      restart (removes all services and restart them -- without rebuilding)"
+	@echo "	   make build:		  build (usually, requires to build when changes happen to the Dockerfile or docker-compose.yml)"
+	@echo "    make bash: 		  open bash console in the app service"
+	@echo "    make test: 		  run application unit tests"
+	@echo "    make shell_plus:   access shell"
+
+
+lint:
+	docker-compose -f local.yml run --rm api pre-commit run --all-files
+
+build:
+	docker compose -f local.yml up --build -d --remove-orphans
+
+up:
+	docker compose -f local.yml up
+
+upd:
+	docker compose -f local.yml up -d
+
+rm:
+	docker compose -f local.yml down && docker compose -f local.yml rm -f
+
+down:
+	docker compose -f local.yml down
+
+ps:
+	docker-compose ps
+
+bash:
+	docker compose f local.yml run --rm api bash
+
+show_logs:
+	docker compose -f local.yml logs
+
+migrate:
+	docker compose -f local.yml run --rm api python3 manage.py migrate
+
+migrations:
+	docker compose -f local.yml run --rm api python3 manage.py makemigrations
+
+collectstatic:
+	docker compose -f local.yml run --rm api python3 manage.py collectstatic --no-input --clear
+
+superuser:
+	docker compose -f local.yml run --rm api python3 manage.py createsuperuser
+
+restart:
+	make rm && make up
+
+flake8:
+	docker compose -f local.yml exec api flake8 .
+
+black:
+	docker compose -f local.yml exec api black --exclude=migrations .
---
+++ core_project/settings/base.py
@@ -0,0 +1,157 @@
+from pathlib import Path
+
+import environ
+
+env = environ.Env()
+
+ROOT_DIR = Path(__file__).resolve().parent.parent.parent
+
+APPS_DIR = ROOT_DIR / "core_apps"
+
+DEBUG = env.bool("DJANGO_DEBUG", False)
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+DJANGO_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+THIRD_PARTY_APPS = [
+    "rest_framework",
+    "djoser",
+    "rest_framework_simplejwt",
+]
+
+LOCAL_APPS = [
+    "core_apps.common",
+    "core_apps.users",
+    "core_apps.profiles",
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'core_project.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [str(APPS_DIR / "templates")],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'core_project.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+DATABASES = {"default": env.db("DATABASE_URL")}
+DATABASES["default"]["ATOMIC_REQUESTS"] = True
+DATABASES["default"]["ENGINE"] = "django.db.backends.postgresql"
+
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+]
+
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+SITE_ID = 1
+
+ADMIN_URL = "admin/"
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = str(ROOT_DIR / 'staticfiles')
+STATICFILES_DIRS = []
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
+
+MEDIA_URL = "/mediafiles/"
+MEDIA_ROOT = str(ROOT_DIR / "mediafiles")
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(name)-12s %(asctime)s %(module)s "
+            "%(process)d %(thread)d %(message)s"
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        }
+    },
+    "root": {"level": "INFO", "handlers": ["console"]},
+}
