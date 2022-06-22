03ece6cfb5916bff0b2d00b5c7f17ce4d293cb51
marouane-youssfi10 <marouaneyoussfi5@gmail.com>
2022-06-22 02:39:38 +0100

    implement djoser authentications and create a serializers od users

---
+++ core_apps/apis/users/serializers.py
@@ -0,0 +1,38 @@
+from django.contrib.auth import get_user_model
+from djoser.serializers import UserCreateSerializer
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    first_name = serializers.SerializerMethodField()
+    last_name = serializers.SerializerMethodField()
+    username = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "first_name", "last_name"]
+
+    def get_first_name(self, obj):
+        return obj.first_name.title()
+
+    def get_last_name(self, obj):
+        return obj.last_name.title()
+
+    def get_full_name(self, obj):
+        first_name = obj.user.first_name.title()
+        last_name = obj.user.last_name.title()
+        return f"{first_name} {last_name}"
+
+    def to_representation(self, instance):
+        representation = super(UserSerializer, self).to_representation(instance)
+        if instance.is_superuser:
+            representation["admin"] = True
+        return representation
+
+
+class CreateUserSerializer(UserCreateSerializer):
+    class Meta(UserCreateSerializer.Meta):
+        model = User
+        fields = ["id", "username", "email", "first_name", "last_name", "password"]
--- core_project/settings/base.py
+++ core_project/settings/base.py
@@ -135,6 +135,53 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "AUTH_HEADER_TYPES": (
+        "Bearer",
+        "JWT",
+    ),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=720),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "SIGNING_KEY": env("SIGNING_KEY"),
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+}
+
+DJOSER = {
+    "LOGIN_FIELD": "email",
+    "USER_CREATE_PASSWORD_RETYPE": True,
+    "USERNAME_CHANGED_EMAIL_CONFIMATION": True,
+    "PASSWORD_CHANGED_EMAIL_CONFIRMATION": True,
+    "SEND_CONFIRMATION_EMAIL": True,
+    "PASSWORD_RESET_CONFIRM_URL": "password/reset/confirm/{uid}/{token}",
+    "SET_PASSWORD_RETYPE": True,
+    "PASSWORD_RESET_CONFIRM_RETYPE": True,
+    "USERNAME_RESET_CONFIRM_URL": "email/reset/confirm/{uid}/{token}",
+    "ACTIVATION_URL": "activate/{uid}/{token}",
+    "SEND_ACTIVATION_EMAIL": True,
+    "SERIALIZERS": {
+        "user_create": "core_apps.apis.users.serializers.CreateUserSerializer",
+        "user": "core_apps.apis.users.serializers.UserSerializer",
+        "current_user": "core_apps.apis.users.serializers.UserSerializer",
+        "user_delete": "djoser.serializers.UserDeleteSerializer",
+    },
+}
+
+# SMTP configuration
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = env("EMAIL_PORT", cast=int)
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = env("EMAIL_USE_TLS", cast=bool)
+
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
