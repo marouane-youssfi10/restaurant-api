3f9a99d5e31e4c263117a3e0d9ea400895463a6f
marouane-youssfi10 <marouaneyoussfi5@gmail.com>
2022-06-23 16:32:19 +0100

    create Customer table in profiles app

--- core_apps/core/profiles/models.py
+++ core_apps/core/profiles/models.py
@@ -1,1 +1,36 @@
-# Create your models here.
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django_countries.fields import CountryField
+from phonenumber_field.modelfields import PhoneNumberField
+
+from core_apps.core.common.models import TimeStampedUUIDModel
+
+User = get_user_model()
+
+
+class Customer(TimeStampedUUIDModel):
+    class Gender(models.TextChoices):
+        MALE = "male", _("male")
+        FEMALE = "female", _("female")
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    gender = models.CharField(
+        verbose_name=_("gender"),
+        choices=Gender.choices,
+        max_length=20,
+        null=True,
+        blank=True,
+    )
+    phone_number = PhoneNumberField(verbose_name=_("phone number"), max_length=30)
+    address = models.CharField(
+        verbose_name=_("address"), max_length=100, null=True, blank=True
+    )
+    country = CountryField(verbose_name=_("country"), blank=False, null=False)
+    city = models.CharField(
+        verbose_name=_("city"), max_length=50, blank=False, null=False
+    )
+    profile_photo = models.ImageField(verbose_name=_("profile photo"))
+
+    def __str__(self):
+        return f"{self.user.username}"
