5c7043b4ce9bcb8625e555c938ee655ca87f8c2c
healingfields <dihratene@gmail.com>
2022-06-25 03:02:42 +0100

    reformated migration

--- core_apps/core/cart/migrations/0001_initial.py
+++ core_apps/core/cart/migrations/0001_initial.py
@@ -11,25 +11,49 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('menu', '0001_initial'),
+        ("menu", "0001_initial"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Cart',
+            name="Cart",
             fields=[
-                ('pkid', models.BigAutoField(editable=False, primary_key=True, serialize=False)),
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, unique=True)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
-                ('quantity', models.IntegerField(default=1)),
-                ('food', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='foods', to='menu.food', verbose_name='food')),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='cart', to=settings.AUTH_USER_MODEL, verbose_name='user')),
+                (
+                    "pkid",
+                    models.BigAutoField(
+                        editable=False, primary_key=True, serialize=False
+                    ),
+                ),
+                (
+                    "id",
+                    models.UUIDField(default=uuid.uuid4, editable=False, unique=True),
+                ),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+                ("quantity", models.IntegerField(default=1)),
+                (
+                    "food",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="foods",
+                        to="menu.food",
+                        verbose_name="food",
+                    ),
+                ),
+                (
+                    "user",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="cart",
+                        to=settings.AUTH_USER_MODEL,
+                        verbose_name="user",
+                    ),
+                ),
             ],
             options={
-                'ordering': ['-created_at', '-updated_at'],
-                'abstract': False,
+                "ordering": ["-created_at", "-updated_at"],
+                "abstract": False,
             },
         ),
     ]
