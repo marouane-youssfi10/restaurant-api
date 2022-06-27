4020fb9fe5c822f8c41cfbc1c035fd564e054115
marouane-youssfi10 <marouaneyoussfi5@gmail.com>
2022-06-27 17:00:05 +0100

    create payment admin panel

---
+++ core_apps/core/payments/admin/__init__.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+from core_apps.core.payments.models import payment
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "payment_id",
+        "user",
+        "method",
+        "amount_paid",
+        "status",
+    ]
+    list_display_links = ["id", "payment_id", "user"]
+    list_filter = ["method"]
+
+
+admin.site.register(payment, PaymentAdmin)
