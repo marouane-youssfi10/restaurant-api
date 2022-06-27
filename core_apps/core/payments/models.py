1712c8cc0882477c67d0e2d78a42aafcfae0a9c3
marouane-youssfi10 <marouaneyoussfi5@gmail.com>
2022-06-27 14:49:24 +0100

    create models payment app

--- core_apps/core/payments/models.py
+++ core_apps/core/payments/models.py
@@ -1,1 +1,45 @@
-# Create your models here.
+import uuid
+
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from core_apps.core.payments.managers import PaymentManager
+
+User = get_user_model()
+
+
+class payment(models.Model):
+    class PaymentMethods(models.TextChoices):
+        paypal = "paypal", _("paypal")
+        stripe = "stripe", _("stripe")
+
+    class Statuses(models.TextChoices):
+        SUCCESSFUL = "successful"
+        FAILED = "failed"
+
+    payment_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_payment"
+    )
+
+    method = models.CharField(
+        verbose_name=_("Method payment"),
+        choices=PaymentMethods.choices,
+        max_length=20,
+        null=False,
+        blank=False,
+    )
+    amount_paid = models.CharField(max_length=100)
+    status = models.CharField(
+        verbose_name=_("Method payment"),
+        choices=PaymentMethods.choices,
+        max_length=20,
+        null=False,
+        blank=False,
+    )
+
+    objects = PaymentManager()
+
+    def __str__(self):
+        return f"{self.user.username}'s payment"
