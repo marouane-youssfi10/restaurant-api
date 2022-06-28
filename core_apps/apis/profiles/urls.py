0db28bd28544e719067037d56c267be835bd0428
marouane-youssfi10 <marouaneyoussfi5@gmail.com>
2022-06-28 23:13:45 +0100

    test listing api all customers

--- core_apps/apis/profiles/urls.py
+++ core_apps/apis/profiles/urls.py
@@ -3,11 +3,11 @@ from django.urls import path, include
 from core_apps.apis.profiles.views import CustomersView, CustomerView
 from core_apps.utils.drf_routers import CustomRouter
 
-app_name = "profile"
+app_name = "customers"
 
 router = CustomRouter(trailing_slash=False)
-router.register(r"all", CustomersView, basename="customers")
-router.register(r"me", CustomerView, basename="customer")
+router.register(r"all", CustomersView, basename="profiles")
+router.register(r"me", CustomerView, basename="profile")
 
 
 urlpatterns = [
