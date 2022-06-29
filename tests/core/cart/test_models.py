0d52dc113f2d5c570ad382fea8e27d5ba61a72b0
marouane-youssfi10 <marouaneyoussfi5@gmail.com>
2022-06-29 21:40:52 +0100

    fix the issue total_price of test cart app

--- tests/core/cart/test_models.py
+++ tests/core/cart/test_models.py
@@ -1,11 +1,10 @@
 import pytest
 
+from tests.core.cart.factories import CartFactory
+
 
 @pytest.mark.django_db
-@pytest.mark.skip(reason="Create CartFactory")
 def test_cart__total_price(user, food):
-    pass
-    # cart = CartFactory(user=user, food=food, quantity=3)
-    # assert cart.quantity == 3
-    # sub_total1 = cart.food.price * cart.quantity
-    # assert cart.sub_total == sub_total1
+    cart = CartFactory(user=user, food=food, quantity=3)
+    assert cart.quantity == 3
+    assert cart.sub_total() == cart.food.price * cart.quantity
