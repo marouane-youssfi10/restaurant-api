779c2f9a4cbdcd333884c52522d86a53d663066d
marouane-youssfi10 <marouaneyoussfi5@gmail.com>
2022-06-30 01:07:10 +0100

    testing create & listing rest api cart app

---
+++ tests/apis/cart/test_cartitem__listing.py
@@ -0,0 +1,12 @@
+import pytest
+from django.urls import reverse
+from rest_framework import status
+
+
+@pytest.mark.django_db
+def test_cartitem_listing(api_client, user):
+    api_client.force_authenticate(user)
+    url = reverse("carts:cartitems-list")
+    assert url == "/api/carts/cartitems"
+    response = api_client.get(url)
+    assert response.status_code == status.HTTP_200_OK, response.content
