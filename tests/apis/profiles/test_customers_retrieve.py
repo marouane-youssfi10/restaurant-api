1e1d91c9ee75145197bbf36b41f7d4bfd85254a5
marouane-youssfi10 <marouaneyoussfi5@gmail.com>
2022-06-30 18:09:35 +0100

    add update & retrieve test for profiles app

---
+++ tests/apis/profiles/test_customers_retrieve.py
@@ -0,0 +1,24 @@
+import pytest
+from django.urls import reverse
+from rest_framework import status
+
+from tests.core.profiles.factories import CustomerFactory
+
+
+@pytest.mark.django_db
+def test_category_retrieve(api_client, user):
+    customer = CustomerFactory(user=user)
+    url = reverse(
+        "customers:profiles-detail",
+        args=[
+            customer.pkid,
+        ],
+    )
+    assert url == f"/api/profiles/{customer.pkid}/"
+    api_client.force_authenticate(user)
+    response = api_client.get(url)
+    assert response.status_code == status.HTTP_200_OK, response.content
+    assert response.json()["customers"]["full_name"] == customer.user.get_full_name
+    assert response.json()["customers"]["gender"] == customer.gender
+    assert response.json()["customers"]["city"] == customer.city
+    assert response.json()["customers"]["user_info"]["email"] == customer.user.email
