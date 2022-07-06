f6b4927cf479ca13202143c284f07fa3975ac72a
marouane-youssfi10 <marouaneyoussfi5@gmail.com>
2022-07-06 16:57:26 +0100

    customzing FoodView views

--- core_apps/apis/menu/views.py
+++ core_apps/apis/menu/views.py
@@ -41,9 +41,6 @@ class FoodView(
         category = self.request.query_params.get("category_slug")
         return queryset.filter(category__slug=category)
 
-    def retrieve(self, request, *args, **kwargs):
-        return super().retrieve(request, *args, **kwargs)
-
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
 
