d9772bfdf3746fd43051b014d29ea10bb231159e
marouane-youssfi10 <marouaneyoussfi5@gmail.com>
2022-07-12 02:23:35 +0100

    add unit test for listing food by category

--- tests/apis/menu/test_food_listing.py
+++ tests/apis/menu/test_food_listing.py
@@ -14,14 +14,14 @@ def test_listing_food_by_name(api_client, user, food):
         "menu:foods-list",
     )
     assert url == f"/api/menu/foods/"
-    response = api_client.get(url + str("?food_name=" + food_image.food.food_name))
-    assert response.status_code == status.HTTP_200_OK, response.content
-    assert "results" in response.json()
-    assert response.json()["count"] == 1
-    assert response.json()["results"][0]["food_name"] == food_image.food.food_name
-    assert response.json()["results"][0]["slug"] == food_image.food.slug
-    assert response.json()["results"][0]["price"] == food_image.food.price
-    assert response.json()["results"][0]["food_image"] == str("/mediafiles/") + str(
+    response1 = api_client.get(url + str("?food_name=" + food_image.food.food_name))
+    assert response1.status_code == status.HTTP_200_OK, response1.content
+    assert "results" in response1.json()
+    assert response1.json()["count"] == 1
+    assert response1.json()["results"][0]["food_name"] == food_image.food.food_name
+    assert response1.json()["results"][0]["slug"] == food_image.food.slug
+    assert response1.json()["results"][0]["price"] == food_image.food.price
+    assert response1.json()["results"][0]["food_image"] == str("/mediafiles/") + str(
         food_image.food_images
     )
     # check the image is none
@@ -32,6 +32,18 @@ def test_listing_food_by_name(api_client, user, food):
         "menu:foods-list",
     )
     assert url == f"/api/menu/foods/"
-    response = api_client.get(url + str("?food_name="))
-    assert response.status_code == status.HTTP_200_OK, response.content
-    assert response.json()["results"][0]["food_image"] is None
+    response2 = api_client.get(url + str("?food_name="))
+    assert response2.status_code == status.HTTP_200_OK, response2.content
+    assert response2.json()["results"][0]["food_image"] is None
+
+
+@pytest.mark.django_db
+def test_listing_food_by_category(api_client, user, food):
+    url = reverse(
+        "menu:foods-list",
+    )
+    assert url == f"/api/menu/foods/"
+    response = api_client.get(url + str("?category_slug=" + food.category.slug))
+    assert "results" in response.json()
+    assert response.json()["count"] == 1
+    assert response.json()["results"][0]["slug"] == food.slug
