44a7239e04ce78605bf8a762013347506a4a6f54
marouane-youssfi10 <marouaneyoussfi5@gmail.com>
2022-07-12 13:25:20 +0100

    remove managment folder from common app & add unit test to customer update

--- tests/apis/profiles/test_customers_listing.py
+++
@@ -1,8 +0,0 @@
-# @pytest.mark.django_db
-# def test_customers_listing(api_client, user):
-#     api_client.force_authenticate(user)
-#     url = reverse("customers:profiles-list")
-#     assert url == "/api/profiles/"
-#     response = api_client.get(url)
-#     assert response.status_code == status.HTTP_200_OK, response.content
-#     assert "customers" in response.json()
--- tests/apis/profiles/test_customers_update.py
+++ tests/apis/profiles/test_customers_update.py
@@ -1,32 +1,46 @@
-# @pytest.mark.django_db
-# def test_address_update(api_client, user, food):
-#     customer = CustomerFactory(user=user, phone_number="21264126212")
-#     url = reverse(
-#         "customers:profiles-detail",
-#         args=[
-#             customer.pkid,
-#         ],
-#     )
-#     assert url == f"/api/profiles/{customer.pkid}/"
-#     api_client.force_authenticate(user)
-#     response = api_client.patch(
-#         url,
-#         data={
-#             "city": "meknes",
-#         },
-#     )
-#     assert response.status_code == status.HTTP_200_OK, response.content
-#     assert response.json()["customers"]["city"] == "meknes"
-#     customer.refresh_from_db()
-#     assert customer.city == response.json()["customers"]["city"]
-# response = api_client.put(
-#     url,
-#     data={
-#         "gender": "female",
-#         "phone_number": "+21262612744",
-#         "address": "street 1 updated ",
-#         "country": "fes",
-#         "city": "meknes updated"
-#     }
-# )
-# assert response.status_code == status.HTTP_200_OK, response.content
+import pytest
+from django.urls import reverse
+from rest_framework import status
+
+from tests.core.profiles.factories import CustomerFactory
+
+
+@pytest.mark.django_db
+def test_customer_update(api_client, user, food):
+    customer1 = CustomerFactory(user=user, phone_number="21264126212")
+    url = reverse(
+        "customers:profiles-detail",
+        args=[
+            customer1.pkid,
+        ],
+    )
+    assert url == f"/api/profiles/{customer1.pkid}/"
+    api_client.force_authenticate(user)
+    # check partial_update customer profile
+    customer2 = CustomerFactory()
+    response = api_client.patch(
+        url,
+        data={
+            "city": customer2.city,
+        },
+    )
+    assert response.status_code == status.HTTP_200_OK, response.content
+    assert response.json()["customer"]["city"] == customer2.city
+    # check update customer profile
+    customer3 = CustomerFactory(phone_number="+41524204242", country="Morocco")
+    response = api_client.put(
+        url,
+        data={
+            "gender": customer3.gender,
+            "phone_number": customer3.phone_number,
+            "address": customer3.address,
+            "country": customer3.country,
+            "city": customer3.city,
+        },
+    )
+    assert response.status_code == status.HTTP_200_OK, response.content
+    assert response.json()["customer"]["gender"] == customer3.gender
+    assert response.json()["customer"]["phone_number"] == customer3.phone_number
+    assert response.json()["customer"]["address"] == customer3.address
+    assert response.json()["customer"]["country"] == customer3.country
+    assert response.json()["customer"]["city"] == customer3.city
