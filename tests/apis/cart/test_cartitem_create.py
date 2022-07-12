7101af13e302b22e5fadb3bb99780c60f53f511c
marouane-youssfi10 <marouaneyoussfi5@gmail.com>
2022-07-12 02:04:02 +0100

    add unit test for create cart

--- tests/apis/cart/test_cartitem_create.py
+++ tests/apis/cart/test_cartitem_create.py
@@ -10,12 +10,27 @@ def test_cart_create(api_client, user, food):
     url = reverse("carts:cartitems-list")
     assert url == "/api/carts/cartitems/"
     api_client.force_authenticate(user)
-    response = api_client.post(
+    response1 = api_client.post(
         url,
         data={"user": user.pkid, "food": food.pkid, "quantity": 2},
     )
-    assert response.status_code == status.HTTP_201_CREATED, response.content
-    assert response.json()["user"] == str(user)
-    assert response.json()["food"] == str(food)
-    assert response.json()["quantity"] == 2
+    assert response1.status_code == status.HTTP_201_CREATED, response1.content
+    assert response1.json()["user"] == str(user)
+    assert response1.json()["food"] == str(food)
+    assert response1.json()["quantity"] == 2
     assert Cart.objects.all().count() == 1
+    response2 = api_client.post(
+        url,
+        data={"user": user.pkid, "food": food.pkid, "quantity": 1},
+    )
+    assert response2.status_code == status.HTTP_201_CREATED, response2.content
+    # check if the new cart is increased not created new one separate
+    assert Cart.objects.all().count() == 1
+
+    # check the quantity field is required
+    response3 = api_client.post(
+        url,
+        data={"user": user.pkid, "food": food.pkid},
+    )
+    assert response3.status_code == status.HTTP_400_BAD_REQUEST, response3.content
+    assert response3.json()["quantity"] == ["This field is required."]
--- tests/apis/cart/test_cartitem_listing.py
+++ tests/apis/cart/test_cartitem_listing.py
@@ -2,9 +2,14 @@ import pytest
 from django.urls import reverse
 from rest_framework import status
 
+from tests.core.cart.factories import CartFactory
+from tests.core.menu.factories import FoodGalleryFactory
+
 
 @pytest.mark.django_db
-def test_cartitem_listing(api_client, user):
+def test_cartitem_listing(api_client, user, food):
+    cart = CartFactory(user=user, food=food)
+    FoodGalleryFactory.create(with_image=True, food=cart.food)
     api_client.force_authenticate(user)
     url = reverse("carts:cartitems-list")
     assert url == "/api/carts/cartitems/"
